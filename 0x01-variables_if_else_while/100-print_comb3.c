197231dcbf868d4b4ed0e44c7ed0afe254e70327
LAFTAOUI Hajar <laftaouihajar58@gmail.com>
2023-10-21 14:41:48 -0700

    second

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int i = 48 ; i < 58 ; i++)
+{
+for (int j = i + 1 ; j < 58 ; j++)
+{
+putchar(i);
+putchar(j);
+if (i < 56)
+{
+putchar(44);
+putchar(32);
+};
+}
+}
+putchar('\n');
+return (0);
+}
