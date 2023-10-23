d744769a8b06dcacbf684a41a0dfb31741a30514
LAFTAOUI Hajar <laftaouihajar58@gmail.com>
2023-10-22 18:25:32 -0700

    third

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  _isalpha -   member 'c'-return value of '_isalpha' 0 1
+*
+*/
+int _isalpha(int c)
+{
+  if ((c <= 'z' &&  c >= 'a') || (c <= 'Z' && c >= 'A'))
+return (1);
+else
+return (0);
+}
